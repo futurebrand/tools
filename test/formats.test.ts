81636721edb7e20d4510223ef7f31d5983b0c769
Gerson Lima <gersonthiago.lima@gmail.com>
2022-09-02 19:33:58 +0100

    test: add camelize test

--- test/formats.test.ts
+++ test/formats.test.ts
@@ -1,8 +1,19 @@
-import { slugify } from '../src/formats';
+import { slugify, camelize } from '../src/formats';
 
 describe('formats', () => {
-  it('should slugify text', () => {
-    expect(slugify('Hello World')).toBe('hello-world');
-    expect(slugify('AAAAAAAAAA bbb CCcCc')).toBe('aaaaaaaaaa-bbb-ccccc');
+  describe('slugify', () => {
+    it('should slugify text', () => {
+      expect(slugify('Hello World')).toBe('hello-world');
+      expect(slugify('AAAAAAAAAA bbb CCcCc')).toBe('aaaaaaaaaa-bbb-ccccc');
+      expect(slugify('Developers Weekly Talks')).toBe('developers-weekly-talks');
+    });
   });
+
+  describe('camelize', () => {
+    it('should camelize text', () => {
+      expect(camelize('hello-world')).toBe('helloWorld');
+      expect(camelize('Hello World')).toBe('helloWorld');
+      expect(camelize('Hello World ')).toBe('helloWorld');
+    })
+  })
 });
